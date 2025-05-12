59c7fbb13f01d7180bb1f2051df009a16fe6b8db
muradcade <king12murad@gmail.com>
2025-05-12 17:33:12 +0300

    switched from composer v1 to composer v2.0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'etech-online-academy/auth-package',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'etech-online-academy/simple-package',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8974a65c4911b583a46fa29309df9e7a59a5de6f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'etech-online-academy/auth-package' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'etech-online-academy/simple-package' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8974a65c4911b583a46fa29309df9e7a59a5de6f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
